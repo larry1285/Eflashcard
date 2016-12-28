bab66156a566f5f002b509591ff5186e6015d419
larry1285 <larry1285@gmail.com>
2016-12-28 17:37:52 +0800

    nothing

--- includes/db.php
+++ includes/db.php
@@ -4,8 +4,8 @@ $db['db_host'] = "us-cdbr-iron-east-04.cleardb.net";
 $db['db_user'] = "b416775ee3bbd5";
 $db['db_pass'] = "f18a7ad1";
 $db['db_name'] = "heroku_fbd7696cb891e27";
-echo "wthuf";
-echo $db['db_host'];
+//echo "wthuf";
+//echo $db['db_host'];
 foreach($db as $key => $value){
   define(strtoupper($key),$value);
 }
