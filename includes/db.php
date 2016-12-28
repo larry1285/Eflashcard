8eaa2b10aeedbbc94e6a23da3640bc5305504808
larry1285 <larry1285@gmail.com>
2016-12-28 15:35:37 +0800

    db

--- includes/db.php
+++ includes/db.php
@@ -1,23 +1,35 @@
 <?php
+$url = parse_url(getenv("mysql://b416775ee3bbd5:f18a7ad1@us-cdbr-iron-east-04.clear"));
 
-$db['db_host'] = 'localhost';
-$db['db_user'] = 'root';
-$db['db_pass'] = '';
-$db['db_name'] = 'eflashcard';
-
-
+$db['db_host'] = $url["host"];
+$db['db_user'] = $url["user"];
+$db['db_pass'] = $url["pass"];
+$db['db_name'] = substr($url["path"], 1);
 foreach($db as $key => $value){
   define(strtoupper($key),$value);
 }
-
-
+$conn = new mysqli($server, $username, $password, $db);
 $connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME); //return ture or false 
-if($connection){
+if(!$connection){die("Unable to connect to db");}
+?>
+<?php
 
-//echo "we are connected" . "<br>";
+//$db['db_host'] = 'localhost';
+//$db['db_user'] = 'root';
+//$db['db_pass'] = '';
+//$db['db_name'] = 'eflashcard';
+//
+//
+//foreach($db as $key => $value){
+//  define(strtoupper($key),$value);
+//}
+//
+//
+//$connection = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME); //return ture or false 
+//if($connection){
+//
+//
+//}
 
-}
-//https://www.youtube.com/watch?v=aAoYaZzWRgw
-//import and export db
 ?>
 
